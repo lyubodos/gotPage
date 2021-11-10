9e2d4e54e4445374477a5ddda40e032040aadc23
Vasilev <lyubimir.vasilev@hpe.com>
2021-11-10 17:05:19 +0100

    Add link for navigation to the heroes and houses sections

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import "./Home.css";
 
@@ -10,7 +11,12 @@ export default class Home extends Component {
                 <div>
                     <img src="https://aniportalimages.s3.amazonaws.com/media/details/GOTTTT_showws.jpg" alt="got-photo"/>
                     <p className="main-para">This is a site providing basic information and stats for each of the GoT heroes and houses. The site's purpouse is to expand the knowledge regarding each of the GoT's universe identity and story
-                    . The site will try to provide the best user experience for every newcomer and for every veteran in the GoT world. Each registered user will be able to try and add an additional story to the character's one, provided that he/she is adding a verified and legitimate information which is connection with the <b>Ice and Fire Novel</b>.</p>
+                    . The site will try to provide the best user experience for every newcomer and for every veteran in the GoT world. Each registered user will be able to try and add an additional story to the character's one, provided that he/she is adding a verified and legitimate information which is connection with the <b>Ice and Fire</b> novel.</p>
+                </div>
+                <div className="nav-container">
+                <Link to="/heroes"  className="nav-container_link">Heroes</Link>
+                <Link to="/houses"  className="nav-container_link">Houses</Link>
+
                 </div>
             </div>
         )
