0a5e39812d181fc8a647b63fa33f18cfa18cfc82
Lyubo <lyubomir.vasilev1992u@gmail.com>
2021-11-10 18:21:14 +0200

    Add a Footer component
    Added a Footer component for all of the pages on the site.
    
    Footer is the baisc part of every UI/UX.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import "./Footer.css";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer-container">
+                <p>Smart Fox &copy; All rights reserved.</p>
+            </div>
+        )
+    }
+}
