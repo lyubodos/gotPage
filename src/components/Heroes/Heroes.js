c95258868c685a2dd1a6461507493aba53268f30
Vasilev <lyubimir.vasilev@hpe.com>
2021-11-15 22:16:24 +0100

    Change the search field implementation
    The search field components was removed and the form for it's
    functionality was pasted in the Heroes component
    
    Change was needed due to child to parent incompatiblity , which was
    causign the searc field not to function properly.

--- src/components/Heroes/Heroes.js
+++ src/components/Heroes/Heroes.js
@@ -10,15 +10,17 @@ export default class Heroes extends Component {
     constructor(props) {
         super(props);
 
+        this.filterHeroes = this.filterHeroes.bind(this);
+
         this.state = {
-            heroes: []
+            heroes: [],
+            searchInp: ""
         };
     };
 
     componentDidMount() {
         axios.get("http://localhost:4000/heroes")
             .then(res => {
-                console.log(res.data)
                 this.setState({
                     heroes: res.data
                 })
@@ -26,12 +28,47 @@ export default class Heroes extends Component {
             .catch(err => console.log(err));
     };
 
+    filterHeroes(e){
+        e.preventDefault();
+
+        axios.get("http://localhost:4000/heroes")
+            .then(res => {
+                if(this.state.searchInp === ""){
+                    return this.state.heroes;
+                }
+
+                this.setState({
+                    heroes: res.data.filter(hero => {
+                        const heroName = hero.name.toLowerCase();
+
+                        return heroName.includes(this.state.searchInp.toLowerCase())
+                    })
+                });
+                console.log(this.state.heroes);
+
+            })
+
+    };
+
 
     render() {
         return (
             <div className="heroes-container">
                 <h1>Heroes:</h1>
-                <Search/>
+                <form onSubmit={this.filterHeroes}>
+                    <label>Search heroes </label>
+                    <input
+                        className="input-search"
+                        type="text"
+                        id="hero-search"
+                        placeholder="Input hero name"
+                        name="hero-s"
+                        onChange={e => {
+                            this.setState({searchInp: e.target.value});
+                        }} 
+                        />
+                    <button type="submit">Search</button>
+                </form>
                 <div className="row-contain">
                     {this.state.heroes.map(hero => {
                         return <HeroCard key={hero._id} {...hero} />
